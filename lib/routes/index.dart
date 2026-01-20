f7df3503134f9694c83a2ef650576180a77fcdea
sansuiqiegaoleng <sansuiqiegaoleng@gmail.com>
2026-01-20 15:24:39 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+//返回APP根Widget
+Widget getRootWidget() {
+  return MaterialApp(initialRoute: '/', routes: getRootRoutes());
+}
+
+//返回APP路由表
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {'/': (context) => MainPage(), '/login': (context) => LoginPage()};
+}
