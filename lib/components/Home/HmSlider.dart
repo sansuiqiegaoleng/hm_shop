94441a4393a32dd2ddcd38546549791db5338636
sansuiqiegaoleng <sansuiqiegaoleng@gmail.com>
2026-01-21 16:48:38 +0800

    实现轮播图渲染

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,20 +1,38 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  HmSlider({Key? key}) : super(key: key);
+  List<BannerItem> bannerList;
+  HmSlider({Key? key, required this.bannerList}) : super(key: key);
 
   @override
   _HmSliderState createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    final double width = MediaQuery.of(context).size.width;
+    return CarouselSlider(
+      items: List.generate(
+        widget.bannerList.length,
+        (index) => Image.network(
+          widget.bannerList[index].imgUrl ?? "",
+          fit: BoxFit.cover,
+          width: width,
+        ),
+      ),
+      options: CarouselOptions(
+        height: 200,
+        viewportFraction: 1.0,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 5),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      height: 300,
-      color: Colors.blue[200],
-      child: Text("轮播图", style: TextStyle(fontSize: 20, color: Colors.black)),
-    );
+    return Stack(children: [_getSlider()]);
   }
 }
