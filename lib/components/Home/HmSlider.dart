ef972e19704692c90ef283b59f1f67d7399b413f
sansuiqiegaoleng <sansuiqiegaoleng@gmail.com>
2026-01-21 18:10:55 +0800

    完成搜索框和指示灯

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hm_shop/viewmodels/home.dart';
 
+// ignore: must_be_immutable
 class HmSlider extends StatefulWidget {
   List<BannerItem> bannerList;
   HmSlider({Key? key, required this.bannerList}) : super(key: key);
@@ -11,9 +12,18 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  late CarouselSliderController _controller;
+  int _currentIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    _controller = CarouselSliderController();
+  }
+
   Widget _getSlider() {
     final double width = MediaQuery.of(context).size.width;
     return CarouselSlider(
+      carouselController: _controller,
       items: List.generate(
         widget.bannerList.length,
         (index) => Image.network(
@@ -27,12 +37,75 @@ class _HmSliderState extends State<HmSlider> {
         viewportFraction: 1.0,
         autoPlay: true,
         autoPlayInterval: Duration(seconds: 5),
+        onPageChanged: (index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _getSearchBar() {
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          height: 40,
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 0, 0, 0.5),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text("搜索...", style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+  }
+
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 20,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentIndex = index;
+                });
+                _controller.animateToPage(index);
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                height: 6,
+                width: index == _currentIndex ? 40 : 20,
+                decoration: BoxDecoration(
+                  color: _currentIndex == index
+                      ? const Color.fromARGB(255, 198, 125, 149)
+                      : Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getSearchBar(), _getDots()]);
   }
 }
