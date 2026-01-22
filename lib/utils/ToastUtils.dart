03dc4191160e2d6a783cef1ca3b67231720ad6f0
sansuiqiegaoleng <sansuiqiegaoleng@gmail.com>
2026-01-22 19:07:22 +0800

    完成登陆页面搭建和表单校验

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  static bool showloading = false;
   static void show(BuildContext context, String? msg) {
+    if (ToastUtils.showloading) {
+      return;
+    }
+    ToastUtils.showloading = true;
+    Future.delayed(Duration(seconds: 2), () {
+      ToastUtils.showloading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         width: 120,
